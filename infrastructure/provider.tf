34bcbe078e23124e669f70a9a6a2afbb314e156d
mohd_liverpool1@hotmail.co.uk <mohd_liverpool1@hotmail.co.uk>
2024-02-29 05:40:21 +0000

    15

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -2,12 +2,3 @@ provider "aws" {
   profile = "creating-dev"
   region  = "eu-west-2"
 }
-
-
-terraform {
-  backend "s3" {
-    bucket = "codacy-analysis-result-bucket"
-    key = "test2.pem"
-    region = "eu-west-2"
-  }
-}
