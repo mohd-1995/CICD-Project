8a5a9785fea5c6111d3640029f26362b053970e2
mohd_liverpool1@hotmail.co.uk <mohd_liverpool1@hotmail.co.uk>
2024-03-01 03:31:13 +0000

    18

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,5 +1,11 @@
 provider "aws" {
   profile = "creating-dev"
-  region  = "eu-west-2"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "webhost-myaws"
+    key = "testing2.pem"
+    region = "eu-west-2"
+  }
+}
