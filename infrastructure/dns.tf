ad0693e30957e142cb8c9e958fc203c58ebbfcfd
mohd_liverpool1@hotmail.co.uk <mohd_liverpool1@hotmail.co.uk>
2024-03-03 00:41:39 +0000

    15

--- infrastructure/dns.tf
+++ infrastructure/dns.tf
@@ -2,6 +2,9 @@ resource "aws_route53_zone" "domain" {
   name = "moesportfolio.com"
 }
 
+
+
+#output name 
 output "lb_dns_name" {
   description = "The DNS name of the load balancer"
   value = aws_lb.lb.dns_name
