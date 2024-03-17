9fc2ed78cc59ebc8cd5f675174dcc7b814731766
mohd_liverpool1@hotmail.co.uk <mohd_liverpool1@hotmail.co.uk>
2024-03-17 03:07:25 +0000

    3

--- infrastructure/user-data.sh
+++ infrastructure/user-data.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 # Update the operating system and install Docker
-yum update -y
-yum install -y docker
+sudo yum update -y
+sudo yum install -y docker
+
+# Install cronie (provides cron and crontab)
+sudo yum install -y cronie
+
 
 # Start the Docker service
-systemctl start docker
+sudo systemctl start docker
 
 # Ensure the Docker service starts on boot
-systemctl enable docker
+sudo systemctl enable docker
 
 # Add the ec2-user to the Docker group
-usermod -a -G docker ec2-user
+sudo usermod -a -G docker ec2-user
 
 # Adjust permissions on the Docker socket
-chmod 666 /var/run/docker.sock
+sudo chmod 666 /var/run/docker.sock
 
 # Wait for the Docker daemon to be active
 while ! docker info > /dev/null 2>&1; do
@@ -38,7 +42,11 @@ docker run -d --name my-container -p 80:80 mohd1995/testing:latest
 EOF
 
 # Ensure the script is executable
-chmod +x /tmp/update_docker_image.sh
+sudo chmod +x /tmp/update_docker_image.sh
+
+# Ensure the cron service starts on boot and is running
+sudo systemctl enable crond
+sudo systemctl start crond
 
 # Add a cron job to run the script every 5 minutes
 ( crontab -l 2>/dev/null; echo "*/5 * * * * /bin/bash /tmp/update_docker_image.sh >> /var/log/update_docker_image.log 2>&1" ) | crontab -
